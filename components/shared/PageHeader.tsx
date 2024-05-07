3016f5363bcf7692faba0d1ea0797dbd54289c1d
Benmrad Bilel <109316958+BenMradB@users.noreply.github.com>
2024-05-07 14:08:12 +0100

    rsesponsive design

--- components/shared/PageHeader.tsx
+++ components/shared/PageHeader.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
 
 type PageHeaderProps = {
   title: string;
@@ -6,14 +8,31 @@ type PageHeaderProps = {
 };
 
 const PageHeader = ({ title, subTitle }: PageHeaderProps) => {
+  const [showMore, setShowMore] = useState<boolean>(false);
+
   return (
     <div className="w-full flex flex-col gap-y-3 tracking-wider">
       <h1 className="text-4xl font-bold text-slate-50"> {title} </h1>
       {subTitle ? (
-        <p className="leading-8 text-slate-400 text-lg font-normal">
-          {" "}
-          {subTitle}{" "}
-        </p>
+        <>
+          <p className="hidden md:block leading-8 text-slate-400 text-lg font-normal">
+            {" "}
+            {subTitle}{" "}
+          </p>
+          <p className="md:hidden leading-8 text-slate-400 text-lg font-normal flex flex-col w-full items-start gap-y-2 ">
+            {" "}
+            <span>
+              {showMore ? subTitle : `${subTitle.slice(0, 100)} ...`}{" "}
+            </span>
+            <Button
+              variant={"link"}
+              onClick={() => setShowMore((curr) => !curr)}
+            >
+              {" "}
+              {showMore ? "Show Less" : "Show More"}{" "}
+            </Button>
+          </p>
+        </>
       ) : null}
     </div>
   );
